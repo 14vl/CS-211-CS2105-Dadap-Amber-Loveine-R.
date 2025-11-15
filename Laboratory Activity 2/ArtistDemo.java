3c99b3d4a211134d8a43272d273cff23849d1bf8
Amber Dadap <dadapamber@gmail.com>
2025-11-15 14:39:53 +0800

    Add files via upload

---
+++ Laboratory Activity 2/ArtistDemo.java
@@ -0,0 +1,28 @@
+public class ArtistDemo {
+    public static void main(String[] args) {
+        // instance of Artist
+        Artist artist = new Artist("Charlie Puth", "American", 32, "Music");
+        artist.displayInfo();
+
+        System.out.println();
+
+        Singer singer = new Singer("Nayeon Im", "Korean", 29, "Music", Genre.POP);
+        singer.displayInfo();
+
+        System.out.println();
+
+        Painter painter = new Painter("Leonardo da Vinci", "Italian", 67, "Painter", Medium.OIL);
+        painter.displayInfo();
+
+        System.out.println();
+
+        Writer writer = new Writer("Jose Rizal", "Filipino", 35, "Writer", WritingStyle.NONFICTION);
+        writer.displayInfo();
+
+        System.out.println();
+
+        Dancer dancer = new Dancer("Misty Copeland", "American", 38, "Dancer", DanceStyle.BALLET);
+        dancer.displayInfo();
+
+    }
+}
